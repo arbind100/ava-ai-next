ff7586fd48df5efbaa65918ddec69af8f81626d6
arbind100 <arbindbemal@gmail.com>
2025-06-28 15:43:12 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,9 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import './globals.css';
+
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body style={{ margin: 0, background: "#000" }}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
